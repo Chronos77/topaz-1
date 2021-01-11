ffdcaa189c77471abfdb9a99cf60c6b7ec7e7ba6
Sylvain Santucci <sylvain.santucci@gmail.com>
2021-01-11 22:01:53 +0100

    Implement ARISE effect

--- scripts/globals/spells/arise.lua
+++ scripts/globals/spells/arise.lua
@@ -9,5 +9,19 @@ function onMagicCastingCheck(caster, target, spell)
 end
 
 function onSpellCast(caster, target, spell)
-    return 0
+    if (target:isPC()) then
+        target:sendRaise(3)
+        target:setLocalVar("hasRaiseWithArise", 1)
+    else
+        if (target:getName() == "Prishe") then
+            -- CoP 8-4 Prishe
+            target:setLocalVar("Raise", 1)
+            target:entityAnimationPacket("sp00")
+            target:addHP(target:getMaxHP())
+            target:addMP(target:getMaxMP())
+        end
+    end
+    spell:setMsg(tpz.msg.basic.MAGIC_CASTS_ON)
+
+    return 3
 end
